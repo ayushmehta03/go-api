a3a4e1d530f964cb7a7ab72ef729c948a04435dc
Ayush Mehta <ayushmehtabps@gmail.com>
2025-08-31 00:06:20 +0530

    server

--- cmd/go-api/main.go
+++ cmd/go-api/main.go
@@ -1,8 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/ayushmehta03/go-api/internal/config"
+)
+
+
 
 
 func main(){
-	fmt.Println("Api")
+
+	cfg:=config.MustLoad()
+
+
+	router:=http.NewServeMux();
+	router.HandleFunc("GET /",func(w http.ResponseWriter, r *http.Request){
+		w.Write([] byte("Home Page"))
+	})
+	
+	ch:=make(chan os.Signal,1)
+
+	signal.Notify(ch,os.Interrupt,syscall.SIGINT,syscall.SIGTERM)
+
+	server:=http.Server{
+		Addr: cfg.Addr,
+		Handler: router,
+	}
+
+	go func(){
+		err:=	server.ListenAndServe()
+
+if err!=nil{
+	log.Fatal("nternal Server error")
+}
+
+	}()
+
+	<-ch
+
+
+
+
+
+
+
+
 }
